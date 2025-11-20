f83dcb5a133c1ee3d0b975f40293ed9153a17b0a
Morozov Stanislav <libertadintegra@gmail.com>
2025-11-20 13:57:09 +0200

    Initial server and DB setup

--- Domain/Transaction.cs
+++ Domain/Transaction.cs
@@ -20,8 +20,8 @@ namespace Domain
         public decimal Value { get; set; }
         [MaxLength(500)]
         public string? Note { get; set; }
-        public Wallet Wallet { get; set; } = null!;
-        public Currency Currency { get; set; } = null!;
-        public TransactionCategory TransactionCategory { get; set; } = null!;
+        public virtual Wallet Wallet { get; set; } = null!;
+        public virtual Currency Currency { get; set; } = null!;
+        public virtual TransactionCategory TransactionCategory { get; set; } = null!;
     }
 }
