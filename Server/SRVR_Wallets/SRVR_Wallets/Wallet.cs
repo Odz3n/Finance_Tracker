8c0f04f05f376fa25df644563310a84f318f00e0
Sokol Kira <kira.sokol1203@gmail.com>
2025-11-22 16:56:59 +0200

    feat: add WalletService

---
+++ Server/SRVR_Wallets/SRVR_Wallets/Wallet.cs
@@ -0,0 +1,10 @@
+﻿namespace SRVR_Wallets
+{
+    public class Wallet
+    {
+        public int WalletId { get; set; }
+        public int UserId { get; set; }
+        public string Currency { get; set; } = "USD";
+        public decimal Balance { get; set; } = 0;
+    }
+}
---
+++ Server/SRVR_Wallets/SRVR_Wallets/WalletRequests.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+public class CreateWalletRequest
+{
+    public int UserId { get; set; }
+    public string Currency { get; set; }
+}
+
+
+public class WalletOperationRequest
+{
+    public int WalletId { get; set; }
+    public decimal Amount { get; set; }
+}
